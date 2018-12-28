ef2a6cf7b7f25d927e0a14eaefb83d97c212530b
Ben Craumer <bencraumer@gmail.com>
2018-12-28 13:51:40 -0500

    add ALLOWED_HOST and static file dir to prod deploy file

--- baseball_site/deploy_settings/__init__.py
+++ baseball_site/deploy_settings/__init__.py
@@ -2,3 +2,12 @@ from baseball_site.settings import *
 
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG
+
+ALLOWED_HOSTS = [
+    'localhost',
+    '.herokuapp.com',
+]
+
+STATICFILES_DIRS = (
+     os.path.join(BASE_DIR, 'assets'),
+ )
