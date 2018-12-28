81701135016f32399a81026b9c85289a5f70c240
Ben Craumer <bencraumer@gmail.com>
2018-12-28 14:26:25 -0500

    add whitenoise

--- baseball_site/deploy_settings/__init__.py
+++ baseball_site/deploy_settings/__init__.py
@@ -14,8 +14,4 @@ SECRET_KEY = get_env_variable("SECRET_KEY")
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-     os.path.join(BASE_DIR, 'assets'),
- )
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
